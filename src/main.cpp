06e07883a91657b96596a4cf12f93d1c0a4f0424
Syl23 <42908467+Syl23@users.noreply.github.com>
2022-04-24 18:32:11 +0200

    Modification paramètres de la commande

--- src/main.cpp
+++ src/main.cpp
@@ -13,30 +13,36 @@
 using namespace std;
 
 
-
-
 int main(int argc, char const* argv[]) {
 
-	bool uneven = false;
-
-	if (argc != 5) {
-
-		cout << "Il convient de mettre le nom d'un fichier d'entré, de sortie, une taille de découpage et un scale" << endl;
-		cout << "Usage : " << argv[0] << " entree.ppm sortie.ppm split/seuil scale\n";
+	if (argc != 6) {
+		cout <<"Usage even split : "<<endl;
+		cout <<"\t"<< argv[0] << " 0 entree.ppm sortie.ppm nbSplit scale\n";
+		cout <<"Usage un-even split : "<<endl;
+		cout <<"\t"<< argv[0] << " 1 entree.ppm sortie.ppm standard_deviation_threshold scale\n";
 		return 1;
 	}
 
-	Image* imageIn = new Image((char*)argv[1]);
+	bool uneven = atoi(argv[1]);
+	
+
+	Image* imageIn = new Image((char*)argv[2]);
 	vector<Region*> regions;
 
 	if (uneven) {
-		regions = unevenSplit(imageIn, (double)atoi(argv[3]), 16);
+		
+		double seuil = (double)atoi(argv[4]);
+		cout<<"uneven, seuil : "<<seuil<<endl;
+		seuil *= seuil;
+
+		regions = unevenSplit(imageIn, seuil, 1);
 	}
 	else {
-		regions = split(imageIn, atoi(argv[3]));
+		cout<<"even"<<endl;
+		regions = split(imageIn, atoi(argv[4]));
 	}
-	findBestImagesGiga(regions, atoi(argv[4]), !uneven);
-	scale(regions, atoi(argv[4]));
+	findBestImagesGiga(regions, atoi(argv[5]), !uneven);
+	scale(regions, atoi(argv[5]));
 	applyBestImages(regions);
 
 
@@ -46,6 +52,6 @@ int main(int argc, char const* argv[]) {
 
 
 	Image* out = merge(regions);
-	out->writeToFile((char*)argv[2]);
+	out->writeToFile((char*)argv[3]);
 	return 0;
 }
